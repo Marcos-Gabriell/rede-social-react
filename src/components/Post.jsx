a78584f21b1d726e1bccaaeeab7a90bba856a28c
Marcos <marcosgabriel79355.com>
2023-07-08 23:48:40 -0300

    estilização dos comentarios

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,26 +1,8 @@
 import styles from './Post.module.css';
-
+import { Comment } from './Comment';
 export function Post() {
   return (
     <>
-      <article className={styles.post}>      
-        <header>
-          <div className={styles.author}>
-            <img className={styles.avatar} src="https://github.com/Marcos-Gabriell.png" />
-            <div className={styles.authorinfo}>
-              <strong>Marcos Gabriel</strong>
-              <span>Web Developer</span>
-            </div>
-          </div>
-          <time title="07/07/23 às 21:18" dateTime="2023-05-11 ">Publicado há 1h</time>
-        </header>
-        <div className={styles.content}>
-          <p>Fala galeraa 👋</p>
-          <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀 </p>
-          <p>👉 <a href="">jane.design/doctorcare</a></p>
-          <p> <a href="">#novoprojeto #nlw #rocketseat</a></p>
-        </div>
-      </article>
       <article className={styles.post}>      
         <header>
           <div className={styles.author}>
@@ -42,6 +24,23 @@ export function Post() {
             <a href="">#rocketseat </a> {'  '}
          </p>
         </div>
+        
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea
+          placeholder="Deixe um comentário"
+        />
+
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
+      <div className={styles.commentList}>
+        <Comment />
+        <Comment />
+        <Comment />
+      </div>
       </article>
     </>
   );
