96aab4272c5ca10793ec9530456fc50fe4252557
Marcos <marcosgabriel79355.com>
2023-08-11 23:37:16 -0300

    erro em implementra o escopo de deltar comentario

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -3,7 +3,12 @@ import styles from './Comment.module.css';
 
 import { ThumbsUp, Trash } from 'phosphor-react';
 
-export function Comment(props) {
+export function Comment({ content, deleteComment }) {
+  function handleDeleteComment() {
+      console.log('Deletar')
+      deleteComment(content)
+  }
+
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://github.com/Marcos-Gabriell.png"  />
@@ -16,12 +21,12 @@ export function Comment(props) {
               <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">Cerca de 1h atrás</time>
             </div>
 
-            <button title="Deletar comentário">
+            <button onClick={handleDeleteComment} title="Deletar comentário">
               <Trash size={24} />
             </button>
           </header>
 
-          <p>{props.content}</p>
+          <p>{content}</p>
         </div>
 
         <footer>
